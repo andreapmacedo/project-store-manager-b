37174762626830772bd9f0ed59cf795cee5af857
andreapmacedo <aa.andremacedo@gmail.com>
2022-08-11 14:52:34 -0300

    commit inicial, vqv

--- app.js
+++ app.js
@@ -10,4 +10,5 @@ app.get('/', (_request, response) => {
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
 // você deve usar o arquivo index.js para executar sua aplicação 
+
 module.exports = app;
