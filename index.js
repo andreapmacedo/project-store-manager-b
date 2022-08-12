0e86c9decf56aa2dd27baca44fd0d173fee2df32
andreapmacedo <aa.andremacedo@gmail.com>
2022-08-12 06:01:20 -0300

    feito: adiciona arquivo com configuração de conexão

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const app = require('./app');
 require('dotenv').config();
-
+//
 // não altere esse arquivo, essa estrutura é necessária para à avaliação do projeto
+
+// const routes = require('./routes');
 
 app.listen(process.env.PORT, () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
---
+++ models/connection.js
@@ -0,0 +1,29 @@
+const mysql = require('mysql2/promise');
+
+require('dotenv').config(); // carrega as variáveis de ambiente
+
+const {
+    MYSQL_HOST,
+    MYSQL_PORT,
+    MYSQL_USER,
+    MYSQL_PASSWORD,
+    MYSQL_DATABASE,
+  } = process.env;
+
+// Usando o pool de conexões para evitar sobrecarga de conexões 
+const connection = mysql.createPool({
+  host: MYSQL_HOST || 'localhost',
+  port: MYSQL_PORT || 3306, // Na porta, podemos utilizar tanto string quanto número
+  user: MYSQL_USER || 'root',
+  password: MYSQL_PASSWORD || 'password',
+  database: MYSQL_DATABASE || 'processos',
+});
+
+// const connection = mysql.createPool({
+//   host: process.env.MYSQL_HOST,
+//   port: 3306,
+//   user: process.env.MYSQL_USER,
+//   password: process.env.MYSQL_PASSWORD,
+// });
+
+module.exports = connection;
