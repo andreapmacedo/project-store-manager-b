70feef08b9272f3d9c60d07ccc2c0dae6c1bbcf4
andreapmacedo <aa.andremacedo@gmail.com>
2022-08-13 15:13:33 -0300

    corrige: erros de lint

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const productController = require('../controllers/productController');
+const productController = require('../controllers/productsController');
 
 const productRoute = express.Router();
 
